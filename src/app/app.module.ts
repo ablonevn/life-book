1084e44d1ba42faa86690dfe8cfb15ff98e19644
ablonevn <tungbuivn@gmail.com>
2019-08-03 16:34:15 +0700

    8 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeaderComponent } from '../header/header.component';
+import * as mat from '@angular/material';
 
 @NgModule({
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,HeaderComponent ],
+  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule ],
+  declarations: [ AppComponent, HelloComponent,HeaderComponent,mat.MatToolbar,mat.MatButton
+    ,mat.MatRipple ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
