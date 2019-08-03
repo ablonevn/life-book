5b308e6f4edd227c2b489fa48fc592c7b781b9c5
ablonevn <tungbuivn@gmail.com>
2019-08-03 15:51:22 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from '../header/header.component';
 
 @NgModule({
   schemas:[CUSTOM_ELEMENTS_SCHEMA],
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
